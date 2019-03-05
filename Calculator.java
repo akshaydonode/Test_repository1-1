8a40ef1730b57dfa25d7f963cee97cf33048998f
akshay_donode <donodeakshay@gmail.com>
2019-03-05 16:27:31 +0530

    added 22222

--- Calculator.java
+++ Calculator.java
@@ -3,5 +3,6 @@ public class Calculator{
 		System.out.println("Hello world");
 		System.out.println("Calculator java program");
 System.out.println("Changes statement");
+System.out.println("Changes statement22222");
 		}
 }
