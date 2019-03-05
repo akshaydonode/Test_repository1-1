f0d55ebc1bf80b4fb3c3508d259dd4d59aa69d25
akshay_donode <donodeakshay@gmail.com>
2019-03-05 15:52:22 +0530

    Added new sentence

--- Calculator.java
+++ Calculator.java
@@ -2,6 +2,6 @@ public class Calculator{
 	public static void main(String a[]){
 		System.out.println("Hello world");
 		System.out.println("Calculator java program");
-
+System.out.println("Changes statement");
 		}
 }
